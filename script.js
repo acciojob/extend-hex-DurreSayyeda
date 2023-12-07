f06608eed484cef56b15a0f25422d71b5f60cace
Sayyeda Afshan <syed1995.sa@gmail.com>
2023-12-07 20:59:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,20 @@
 const extendHex = (shortHex) => {
   // write your code here
   // Remove # if present and convert to lowercase
-  shortHex = shortHex.replace("#", "").toLowerCase();
-
-  // If the shortHex has only one character per color, duplicate each character
-  if (shortHex.length === 3) {
-    shortHex = shortHex
-      .split("")
-      .map((char) => char.repeat(2))
-      .join("");
-  }
-
-  // If the shortHex has invalid length, return null
-  if (shortHex.length !== 6) {
-    return null;
-  }
-
-  // Prepend # and return the full hex code
-  return "#" + shortHex;
+	let i = shortHex.length - 3,isCapital = false;
+	let output = "#";
+	while(i < shortHex.length){
+		let asciiCode = shortHex[i].charCodeAt(0);
+		if(asciiCode >= 65 && asciiCode <=90){
+			isCapital = true;
+		}
+		// output = "#" += "a" + "a" => "#aa"
+		output += shortHex[i] + shortHex[i];
+		i++;
+	}
+ 
 }
-
-// Examples
-console.log(extendHex("#abc"));   // Output: "#aabbcc"
-console.log(extendHex("abc"));    // Output: "#aabbcc"
-console.log(extendHex("#AbC"));   // Output: "#AABBCC"
-console.log(extendHex("#f09"));   // Output: "#ff0099"
-};
-
+ 
 // Do not change the code below.
 const shortHex = prompt("Enter Short Hex.");
 alert(extendHex(shortHex));
